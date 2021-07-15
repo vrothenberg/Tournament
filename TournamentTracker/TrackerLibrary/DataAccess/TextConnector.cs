f6097c34dfbd456f2fd34668a62fc1e486e2ac4f
Vince Rothenberg <vrothenberg@gmail.com>
2021-07-14 17:24:13 -0700

    Text file DB connector

--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -3,15 +3,38 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
+using System.Linq;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
         // TODO - Wire up the CreatePrize for text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // Load text file and convert text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find the max ID
+
+            int currentId = 1;
+            
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            // Add the new record with new ID (max + 1)
+            prizes.Add(model);
+
+            // Convert the prizes to List<string>
+            // Save the List<string> to text file 
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
